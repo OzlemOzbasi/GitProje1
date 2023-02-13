6ddd49f53facdec1e4aee186548e7386c44b0722
Ozlem <ozbasiozlem@gmail.com>
2023-02-13 22:07:16 +0300

    Öğrenci Class'ı ikinci bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -2,5 +2,9 @@ public class Ogrenci {
     public static void main(String[] args) {
 
         System.out.println("Welcome ... Where have you been, we miss you ...    :))))) ");
+
+
+        System.out.println("Looks like you didn't forget about this     :))))) ");
+
     }
 }
